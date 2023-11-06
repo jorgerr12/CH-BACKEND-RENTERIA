48e53b84e4f9b0ab8f8f90e366d10ff33de065cc
Jorge Renteria <jorge_9_90@hotmail.com>
2023-11-06 11:54:27 -0500

    add documentacion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import  express  from "express"
+import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import session from "express-session"
 import { __dirname } from "./utils.js"
-import hanblebars  from "express-handlebars"
+import hanblebars from "express-handlebars"
 import { config } from "./config/config.js"
-import {viewsRouter} from "./routers/views.routes.js"
+import { viewsRouter } from "./routers/views.routes.js"
 import { connectDB } from "./config/dbConnection.js"
 import { productsRouter } from "./routers/products/products.routes.js"
 import { cartsRouter } from "./routers/cart/cart.routes.js"
@@ -15,10 +15,12 @@ import MongoStore from "connect-mongo"
 import initializePassport from "./config/passport.config.js"
 import passport from "passport"
 import { setLogger } from "./utils/logger.js"
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUiExpress from "swagger-ui-express";
 
 const NODE_ENV = process.env.NODE_ENV || 'development'
 dotenv.config({
-  path:`.env.${NODE_ENV}`
+  path: `.env.${NODE_ENV}`
 })
 
 
@@ -30,14 +32,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.use(
   cors({
-      origin: "*",
-      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-      })
-  );
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  })
+);
 app.use(
   session({
-    store:MongoStore.create({
-      mongoUrl:config.mongo.url,
+    store: MongoStore.create({
+      mongoUrl: config.mongo.url,
       mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
       ttl: 60,
     }),
@@ -55,12 +57,27 @@ app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
 
 const httpServer = app.listen(config.server.port, () => {
-    console.log("server running in port", config.server.port);
-  });
+  console.log("server running in port", config.server.port);
+});
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.1',
+    info: {
+      title: 'Documentación de la API del Ecommerce',
+      description: 'Proyecto de BackEnd CoderHouse',
+      contact: {
+        name: 'Jorge Renteria',
+    },
+    }
+  },
+  apis: [`${__dirname}/docs/**/*.yml`]
+};
+const specs = swaggerJSDoc(swaggerOptions)
+app.use(`/docs`, swaggerUiExpress.serve, swaggerUiExpress.setup(specs))
 
-  connectDB();
+connectDB();
 app.use(viewsRouter);
-app.use("/api/products",productsRouter)
-app.use("/api/carts",cartsRouter);
-app.use("/api/session",sessionRouter)
-app.use("/api/mailing",mailingRouter)
+app.use("/api/products", productsRouter)
+app.use("/api/carts", cartsRouter);
+app.use("/api/session", sessionRouter)
+app.use("/api/mailing", mailingRouter)
